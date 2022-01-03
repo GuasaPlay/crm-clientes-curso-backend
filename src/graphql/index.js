584b2492085dfaf9c47a057fe46c076c72fe9221
Oscar Calle <gameplaysoscar@gmail.com>
2022-01-03 10:55:00 -0500

    Added the import of the default schema to the graphql index

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,10 +1,11 @@
 import { mergeSchemas } from 'graphql-tools'
 
+import defaultSchema from './schemas/default.schema'
 import productSchema from './schemas/product.schema'
 import productResolver from './resolvers/prodcut.resolver'
 
 export default mergeSchemas({
    mergeDirectives: true,
-   schemas: [productSchema],
+   schemas: [defaultSchema, productSchema],
    resolvers: [productResolver],
 })
