2af53f4d3dd46d6ee4cbcbf8be7f3e386a4f9e9e
Oscar Calle <gameplaysoscar@gmail.com>
2022-01-03 10:55:19 -0500

    Added product schema for graphql

--- src/graphql/schemas/product.schema.js
+++ src/graphql/schemas/product.schema.js
@@ -2,9 +2,30 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
    type Query {
-      getProduct: String
+      getProducts: ProductQR!
    }
+
+   type Mutation {
+      createProduct: ProductMR!
+   }
+
    type Product {
-      name: String
+      name: String!
+      stock: Int
+      price: Float!
+   }
+
+   type ProductQR implements QueryResponse {
+      code: String!
+      success: Boolean!
+      message: String!
+      products: [Product]!
+   }
+
+   type ProductMR implements MutationResponse {
+      code: String!
+      success: Boolean!
+      message: String!
+      product: Product
    }
 `
